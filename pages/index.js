a05c57a1294214e42f860f6b0d2c7ffc84692342
Kyle Landon <kylepotteiger@gmail.com>
2023-07-18 06:50:21 +0000

    Test 0.0.1

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+        <Header title="Working on The Website Right Now <3" /> 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
