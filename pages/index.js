f3b4b00fe6e894b38bc04573b478dc746eefe99c
Kyle Landon <kylepotteiger@gmail.com>
2023-07-23 21:52:57 +0000

    adssdf

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,20 @@ export default function Home() {
         <title>Texas Terrain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
+      <navigator>
 
       <img src="/logo+text.png" alt="Logo" class="logo"></img>
-      
-        <div class="navbar">
-          <a href="#">Home</a>
-          <a href="#">Services</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </div>
 
+      <div class="navbar">
+        <a href="#">Home</a>
+        <a href="#">Services</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+      
+      </navigator>
+
+      <main>
         <div class="main_panel">
           <div class="panel_1">
             <Header title="Panel One"/>
@@ -36,16 +38,10 @@ export default function Home() {
           </div>
         </div>
 
-        <Header title="Texas Terrain, LLC" />
-        <Header title="Working on The Website Right Now <3" />
-        <Header title="Scan QR Below to Contact Us!" />
         <img src="/Contact_Info_QR.png" alt="Contact_QR" class="contact_QR"></img>
-
-
-        
         <script id="housecall-pro-chat-bubble" src="https://chat.housecallpro.com/proChat.js" type="text/javascript" data-color="#2378cf" data-organization="1c362d34-771a-42a9-a95e-26a22ca2634a"></script>
       </main>
-
+      
       <Footer />
     </div>
   )
